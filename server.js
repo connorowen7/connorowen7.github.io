9ba69cd22363fef413207f0c8b3d02d1465c380b
connorowen7 <7connor28@comcast.net>
2020-10-20 15:42:58 -0400

    Update server.js
    updated server.js might need to fix again

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
+import countries from '.public/lab_6/countries.js'
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ app.route('/api')
     res.send(`Lab 5 for Connor Owen`);
   })
   .post((req, res) => {
+    res.json(countries);
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
   });
