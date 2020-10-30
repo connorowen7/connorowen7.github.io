233cb10c2d1eaab83b4f02ad2763a9ecd6a45ab1
connorowen7 <7connor28@comcast.net>
2020-10-30 16:48:23 -0400

    Update server.js
    updated server.js agian

--- server.js
+++ server.js
@@ -26,10 +26,12 @@ app.route('/api')
     console.log('GET request detected');
     res.send(`Lab 6 for Connor Owen`);
   })
-  .post((req, res) => {
-    res.json(countries);
+  .post(async (req, res) => {
     console.log('POST request detected');
-    console.log('Form data in res.body', req.body);
+    
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+    res.json(json);
   });
 
 app.listen(port, () => {
